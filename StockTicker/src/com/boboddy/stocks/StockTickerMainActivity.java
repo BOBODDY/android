ea51812d5be000b9949746ecd73b331b177451f6
Nick Mathews <nick@perfectednetworks.com>
2012-06-13 17:31:17 -0400

    Initial commit of StockTicker project

---
+++ StockTicker/src/com/boboddy/stocks/StockTickerMainActivity.java
@@ -0,0 +1,13 @@
+package com.boboddy.stocks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StockTickerMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
