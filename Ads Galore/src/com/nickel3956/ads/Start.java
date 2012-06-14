fd54142e83dcdbe5c3b0fc15aca4863b3ba5471b
Nick Mathews <nick@perfectednetworks.com>
2012-06-14 10:23:39 -0400

    Initial commit of Ads Galore project

---
+++ Ads Galore/src/com/nickel3956/ads/Start.java
@@ -0,0 +1,13 @@
+package com.nickel3956.ads;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Start extends Activity{
+	
+	public void onCreate(Bundle bundle)
+	{
+		super.onCreate(bundle);
+		setContentView(R.layout.main);
+	}
+}
